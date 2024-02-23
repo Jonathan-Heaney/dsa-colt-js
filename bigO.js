65b0798089e7550245753410da8e0378f5dbc2cf
Jonathan Heaney <jonathan.heaney@gmail.com>
2024-02-23 10:35:25 -0600

    Add comments about number of operations

--- bigO.js
+++ bigO.js
@@ -1,17 +1,24 @@
 // Write a function that calculates the sum of all numbers from 1 up to (and including) some number n
 
+// 5n + 2 operations, but we actually just care about the general trend
+// As n grows, the number of operations grows roughly proportionally with n
 function addUpTo(n) {
-  let total = 0;
-  for (let i = 1; i <= n; i++) {
-    total += i;
+  let total = 0; // 1 assignment
+  for (
+    let i = 1 /* 1 assignment */;
+    i <= n /* n comparisons */;
+    i++ /* n additions and n assignments */
+  ) {
+    total += i; // n additions and n assignments. As n grows, there are more assignments
   }
   return total;
 }
 
 console.log(addUpTo(100));
 
+// 3 operations no matter what n is; O is constant
 function addUpToFormula(n) {
-  return (n * (n + 1)) / 2;
+  return (n * (n + 1)) / 2; // 3 operations. It doesn't matter what n is
 }
 
 console.log(addUpToFormula(100));
