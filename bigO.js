54723f06545630cdeb1974340ae94dab929b073a
Jonathan Heaney <jonathan.heaney@gmail.com>
2024-02-23 11:01:08 -0600

    Start another linear example

--- bigO.js
+++ bigO.js
@@ -2,6 +2,7 @@
 
 // 5n + 2 operations, but we actually just care about the general trend
 // As n grows, the number of operations grows roughly proportionally with n
+// So, the big O would be O(n)
 function addUpTo(n) {
   let total = 0; // 1 assignment
   for (
@@ -17,8 +18,12 @@ function addUpTo(n) {
 console.log(addUpTo(100));
 
 // 3 operations no matter what n is; O is constant
+// So the Big O would be: O(1)
 function addUpToFormula(n) {
   return (n * (n + 1)) / 2; // 3 operations. It doesn't matter what n is
 }
 
 console.log(addUpToFormula(100));
+
+// Another linear example - O(n), because O(2n) reduces to O(n), since we only care about orders of magnitude
+
